954be6f3b3c32e87db660719a676723e7b6193d9
rifall88 <rifaldistore9@gmail.com>
2025-07-04 10:22:01 +0700

    DailySchedule

--- src/models/User.js
+++ src/models/User.js
@@ -15,28 +15,6 @@ class User {
       where: { email },
     });
   }
-
-  // Method untuk mencari user berdasarkan ID
-  static async findById(id) {
-    return prisma.user.findUnique({
-      where: { id: parseInt(id) }, // Pastikan ID diubah ke integer
-    });
-  }
-
-  // Method untuk memperbarui user
-  static async update(id, userData) {
-    return prisma.user.update({
-      where: { id: parseInt(id) },
-      data: userData,
-    });
-  }
-
-  // Method untuk menghapus user
-  static async delete(id) {
-    return prisma.user.delete({
-      where: { id: parseInt(id) },
-    });
-  }
 }
 
 export default User;
